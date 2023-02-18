d001d327741cce8b19a5b5a5f8f46990b64fd8f3
Shane Celis <shane.celis@gmail.com>
2023-02-18 00:13:17 -0500

    hack: Tried lots of sampling rates.
    Everything was bad.

--- piano.h
+++ piano.h
@@ -4,8 +4,13 @@ float strtof(const char *restrict nptr, char **restrict endptr);
 #include <stdint.h>
 
 /* gaps in your audio? increase this */
+/* #define SAMPLES_PER_BUFFER (256*8) */
 #define SAMPLES_PER_BUFFER (256*8)
 #define SAMPLES_PER_SECOND 24000
+/* #define SAMPLES_PER_SECOND 22050 */
+/* #define SAMPLES_PER_SECOND 12000 */
+/* #define SAMPLES_PER_SECOND 6000 */
+/* #define SAMPLES_PER_SECOND 44100 */
 
 typedef struct {
   void (*song_free)(void *);
